0e4b47fda166a1ec960d10918c5d4085866fd3f3
Jorge Martinez Jr <jorgemartinezjr0@gmail.com>
2020-01-24 17:46:02 -0500

    clock range fixed

--- sketch.js
+++ sketch.js
@@ -24,12 +24,7 @@ function draw() {
       
     let mapSec = map(s,0,60,0,360);
     let mapMin = map(m,0,60,0,360);
-    let mapHr = null
-    if (h < 13){
-        mapHr = map(h,0,12,0,360);
-    }else{
-        mapHr = map(h%12,0,12,0,360);
-    }
+    let mapHr = (h,0,23,0,360);
 
     translate(400,300);
     rotate(3*HALF_PI);
